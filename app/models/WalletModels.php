6a916df8cc867c8f0d9c7a3174c733acb1621265
muchlass19 <muchlas148@gmail.com>
2023-06-23 08:07:51 +0700

    add database wrapper

--- app/models/WalletModels.php
+++ app/models/WalletModels.php
@@ -3,7 +3,19 @@
 class WalletModels {
     private $name = 'Wallet 1';
 
+    private $table = 'wallets';
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database;
+    }
+
     public function getName() {
         return $this->name;
+    }
+
+    public function getAllWallets() {
+        $this->db->query('SELECT * FROM '.$this->table);
+        return $this->db->resultSet();
     }
 }
